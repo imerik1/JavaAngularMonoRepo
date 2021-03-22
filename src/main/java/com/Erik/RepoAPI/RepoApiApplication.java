c39460a7851f42620e488f5e8dc9264ce339add9
Erik Santana <santanaerik1106@gmail.com>
2021-03-22 15:40:36 -0300

    Innitial Commit

---
+++ src/main/java/com/Erik/RepoAPI/RepoApiApplication.java
@@ -0,0 +1,13 @@
+package com.Erik.RepoAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Erik/RepoAPI/RepoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Erik.RepoAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
