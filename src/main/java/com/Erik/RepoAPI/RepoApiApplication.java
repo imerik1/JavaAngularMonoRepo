1eb867f56aaafb36f787163369b4007e91627b19
Erik Santana <santanaerik1106@gmail.com>
2021-03-23 02:23:56 -0300

    feat: scan entity and component

--- src/main/java/com/Erik/RepoAPI/RepoApiApplication.java
+++ src/main/java/com/Erik/RepoAPI/RepoApiApplication.java
@@ -2,12 +2,25 @@ package com.Erik.RepoAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.Erik.RepoAPI.model.entity")
+@EnableJpaRepositories(basePackages = "com.Erik.repoAPI.model.repository")
+
 public class RepoApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RepoApiApplication.class, args);
-	}
+//    @Bean
+//    public CommandLineRunner run(ClienteRepository repository ) {
+//        return args -> {
+//            Cliente cliente = Cliente.builder().cpf("22855674808").nome("Erik Santana").dataCadastro(LocalDate.now()).build();
+//            repository.save(cliente);
+//        };
+//    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepoApiApplication.class, args);
+    }
 
 }
