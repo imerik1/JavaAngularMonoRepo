7647fd1537f46c8f383a4bd66267bededb5fe582
Erik Santana <santanaerik1106@gmail.com>
2021-03-23 03:27:21 -0300

    fix: add annot valid on put

--- src/main/java/com/Erik/RepoAPI/model/entity/Cliente.java
+++ src/main/java/com/Erik/RepoAPI/model/entity/Cliente.java
@@ -2,8 +2,10 @@ package com.Erik.RepoAPI.model.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
-
+import org.hibernate.validator.constraints.br.CPF;
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.time.LocalDate;
 
@@ -23,9 +25,12 @@ public class Cliente implements Serializable {
     private Integer id;
 
     @Column(nullable = false, length = 50)
+    @NotEmpty(message = "{campo.nome.obrigatorio}")
     private String nome;
 
     @Column(nullable = false, length = 11)
+    @NotNull(message = "{campo.cpf.obrigatorio}")
+    @CPF(message = "campo.cpf.invalido")
     private String cpf;
 
     @Column(name = "data_cadastro", updatable = false)
