3c57762556ff4544afe3de40ebe8b47625c37588
Erik Santana <santanaerik1106@gmail.com>
2021-03-23 02:24:26 -0300

    add: change name

--- src/main/java/com/Erik/RepoAPI/model/repository/ClienteRepository.java
+++ src/main/java/com/Erik/RepoAPI/model/repository/ClienteRepository.java
@@ -1,7 +1,7 @@
 package com.Erik.RepoAPI.model.repository;
 
-import com.Erik.RepoAPI.model.entity.Clientes;
+import com.Erik.RepoAPI.model.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClienteRepository extends JpaRepository<Clientes, Integer> {
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 }
