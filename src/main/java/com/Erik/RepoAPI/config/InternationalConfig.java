9f3452229a5f0dba6c7aaa056ce0c1afa8784e70
Erik Santana <santanaerik1106@gmail.com>
2021-03-23 03:27:01 -0300

    add: config international

---
+++ src/main/java/com/Erik/RepoAPI/config/InternationalConfig.java
@@ -0,0 +1,28 @@
+package com.Erik.RepoAPI.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+import java.util.Locale;
+
+@Configuration
+public class InternationalConfig {
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("message");
+        messageSource.setDefaultEncoding("ISO-8859-1");
+        messageSource.setDefaultLocale(Locale.getDefault());
+        return messageSource;
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean validatorFactoryBean() {
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource());
+        return bean;
+    }
+}
