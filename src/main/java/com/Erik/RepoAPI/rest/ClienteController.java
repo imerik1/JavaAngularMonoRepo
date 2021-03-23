b5589ae2989b9747657225edcdaa411914a9a450
Erik Santana <santanaerik1106@gmail.com>
2021-03-23 03:04:33 -0300

    add: all controllers

--- src/main/java/com/Erik/RepoAPI/rest/ClienteController.java
+++ src/main/java/com/Erik/RepoAPI/rest/ClienteController.java
@@ -4,10 +4,8 @@ import com.Erik.RepoAPI.model.entity.Cliente;
 import com.Erik.RepoAPI.model.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/clientes")
@@ -17,7 +15,36 @@ public class ClienteController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Cliente salvar(Cliente cliente) {
+    public Cliente salvar(@RequestBody Cliente cliente) {
         return repository.save(cliente);
+    }
+
+    @GetMapping("{id}")
+    public Cliente acharPorId(@PathVariable Integer id) {
+        return repository.findById(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND)
+        );
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletar(@PathVariable Integer id) {
+        repository.findById(id).map(client -> {
+            repository.delete(client);
+            return Void.TYPE;
+        }).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND)
+        );
+    }
+
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void atualizar(@PathVariable Integer id, @RequestBody Cliente clienteAtualizado) {
+        repository.findById(id).map(client -> {
+            clienteAtualizado.setId(client.getId());
+            return repository.save(clienteAtualizado);
+        }).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND)
+        );
     }
 }
