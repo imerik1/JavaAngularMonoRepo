573d40ccc77e63b9748e9eee984520fb74dfbaee
Erik Santana <santanaerik1106@gmail.com>
2021-03-25 02:23:35 -0300

    add: integration w list

--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -1,6 +1,8 @@
+import { Observable } from 'rxjs';
 import { ClientesService } from './../../clientes.service';
 import { Cliente } from './../cliente';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-clientes-form',
@@ -11,24 +13,56 @@ export class ClientesFormComponent implements OnInit {
   cliente: Cliente;
   success = false;
   errors: string[];
+  id: number;
 
-  constructor(private service: ClientesService) {
+  constructor(
+    private service: ClientesService,
+    private activatedRoute: ActivatedRoute
+  ) {
     this.cliente = new Cliente();
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const params: Observable<Params> = this.activatedRoute.params;
+    params.subscribe((urlParams) => {
+      this.id = urlParams[`id`];
+      if (this.id) {
+        this.service.getClienteById(this.id).subscribe(
+          (response) => {
+            this.cliente = response;
+          },
+          (errorMessage) => {
+            this.cliente = new Cliente();
+          }
+        );
+      }
+    });
+  }
 
   onSubmit() {
-    this.service.postCliente(this.cliente).subscribe(
-      (response) => {
-        this.success = true;
-        this.errors = null;
-        this.cliente = response;
-      },
-      (errorResponse) => {
-        this.success = false;
-        this.errors = errorResponse.error.errors;
-      }
-    );
+    if (this.cliente.id) {
+      this.service.putCliente(this.cliente, this.id).subscribe(
+        (response) => {
+          this.success = true;
+          this.errors = null;
+        },
+        (errorMessage) => {
+          this.errors = ['Erro ao atualizar cliente.'];
+          this.success = false;
+        }
+      );
+    } else {
+      this.service.postCliente(this.cliente).subscribe(
+        (response) => {
+          this.success = true;
+          this.errors = null;
+          this.cliente = response;
+        },
+        (errorResponse) => {
+          this.success = false;
+          this.errors = errorResponse.error.errors;
+        }
+      );
+    }
   }
 }
